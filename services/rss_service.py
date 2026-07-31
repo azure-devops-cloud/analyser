ecc09467f00854772b99368429e2d5da402efc12
azure-devops-cloud <84575981+azure-devops-cloud@users.noreply.github.com>
2026-08-01 00:04:48 +0530

    Update rss_service.py

--- services/rss_service.py
+++ services/rss_service.py
@@ -1,1 +1,24 @@
+import feedparser
 
+class RSSService:
+
+    @staticmethod
+    def get_feed(url):
+
+        feed = feedparser.parse(url)
+
+        articles = []
+
+        for entry in feed.entries:
+
+            articles.append({
+
+                "title": entry.get("title"),
+
+                "link": entry.get("link"),
+
+                "published": entry.get("published", "")
+
+            })
+
+        return articles
