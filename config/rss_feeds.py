d4d090fb03385623605cd7d8abb39089e5df06ee
azure-devops-cloud <84575981+azure-devops-cloud@users.noreply.github.com>
2026-08-01 00:04:08 +0530

    Add RSS feeds configuration

---
+++ config/rss_feeds.py
@@ -0,0 +1,14 @@
+RSS_FEEDS = {
+
+    "fed": [
+        "https://www.federalreserve.gov/feeds/press_all.xml"
+    ],
+
+    "markets": [
+        "https://feeds.finance.yahoo.com/rss/2.0/headline"
+    ],
+
+    "crypto": [
+        "https://cointelegraph.com/rss"
+    ]
+}
