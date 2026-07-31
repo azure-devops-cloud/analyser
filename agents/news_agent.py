46bbb4d772bb7e7e5002a5028361ec043f24beac
azure-devops-cloud <84575981+azure-devops-cloud@users.noreply.github.com>
2026-08-01 00:27:54 +0530

    Update news_agent.py

--- agents/news_agent.py
+++ agents/news_agent.py
@@ -3,13 +3,16 @@ from models.agent_result import AgentResult
 
 from config.rss_feeds import RSS_FEEDS
 from services.rss_service import RSSService
+from services.news_storage_service import NewsStorageService
 
 
 class NewsAgent(BaseAgent):
 
+
     def run(self):
 
         articles = []
+
 
         try:
 
@@ -19,6 +22,7 @@ class NewsAgent(BaseAgent):
 
                     results = RSSService.get_feed(feed)
 
+
                     for item in results:
 
                         item["category"] = category
@@ -26,18 +30,34 @@ class NewsAgent(BaseAgent):
                         articles.append(item)
 
 
+
+            storage = NewsStorageService()
+
+
+            new_articles = storage.save_news(
+                articles
+            )
+
+
+            storage.close()
+
+
+
             return AgentResult(
                 agent="news_agent",
                 status="success",
-                data=articles,
-                count=len(articles)
+                data=new_articles,
+                count=len(new_articles)
             )
 
 
         except Exception as error:
 
+
             return AgentResult(
                 agent="news_agent",
                 status="failed",
-                errors=[str(error)]
+                errors=[
+                    str(error)
+                ]
             )
